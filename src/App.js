113373a76d64e114899b001a3b95e30a0052b77f
Jesús García Martínez <jesgarciamartinez@gmail.com>
2021-09-14 12:33:01 +0200

    modify App.js for context explanation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import FriendsApp from './friends-app'
-import StateApp from './3-state-events-classes-hooks'
+// import StateApp from './3-state-events-classes-hooks'
+import ContextApp from './4-context'
 
 function App() {
-  return <StateApp />
+  return <ContextApp />
 }
 
 export default App
